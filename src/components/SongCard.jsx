6653bcab20c87870804c493bd6cdc59b6d805507
Irungu Macharia <machariajosphat001@gmail.com>
2023-06-19 12:14:58 +0000

    youtube redirect functionality works, had to use axios

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import { useState } from "react"; // Import useState hook
-import { Loader } from "../components";
-import { useGetPlaylistDetailsQuery } from "../redux/services/youtubeV3";
+import { useState } from "react";
+import { Loader, Error } from "../components";
+import axios from "axios";
+import { youtubeV3Api } from "../redux/services/youtubeV3";
 
-const SongCard = ({ song, isPlaying, activeSong, data ,i }) => {
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   console.log("Song:\n\n");
   console.log(song);
 
@@ -14,8 +15,16 @@ const SongCard = ({ song, isPlaying, activeSong, data ,i }) => {
   const handleLinkClick = async () => {
     try {
       setIsLoading(true); // Set loading status to true
-      const response = await useGetPlaylistDetailsQuery(song.id.playlistId); // Fetch playlist details
-      setDetails(response); // Update state with playlist details
+      const response = await axios.get(
+        `https://youtube-v31.p.rapidapi.com/playlists?id=${song.id.playlistId}&part=snippet`,
+        {
+          headers: {
+            "X-RapidAPI-Key": "7d336a8b87msh603e2a5ed95cc86p1f2e50jsn98647fd536d8",
+            "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+          },
+        }
+      ); // Fetch playlist details using Axios
+      setDetails(response.data); // Update state with playlist details
     } catch (error) {
       setError(true); // Set error status to true
       console.error(error);
@@ -24,33 +33,45 @@ const SongCard = ({ song, isPlaying, activeSong, data ,i }) => {
     }
   };
 
-  if (isLoading) return <Loader title="Loading links..." />;
+  if (isLoading) return <Loader title="Loading youtube links..." />;
   if (error) return <Error />;
-  
-  const linkUrl = details ? `https://www.youtube.com/watch?v=${details.items[0].id}&list=${song.id.playlistId}&index=1` : '';
+
+  const linkUrl = details
+    ? `https://www.youtube.com/watch?v=${details.items[0].id}&list=${song.id.playlistId}&index=1`
+    : "";
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
-        <div className={`absolute inset-0 justify-center
+        <div
+          className={`absolute inset-0 justify-center
         items-center bg-black bg-opacity-50 group-hover:flex
-        ${activeSong?.title === song.snippet.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`} >
-        </div>
-        <img alt="song_img" src={song.snippet?.thumbnails.high.url}/>
+        ${
+          activeSong?.title === song.snippet.title
+            ? "flex bg-black bg-opacity-70"
+            : "hidden"
+        }`}
+        ></div>
+        <img alt="song_img" src={song.snippet?.thumbnails.high.url} />
       </div>
       <div className="mt-4 flex flex-col">
-        <p className="font-semibold text-lg text-white truncate">
-          <Link to={linkUrl} onClick={handleLinkClick}>
-          {song.snippet.title}
-          </Link>
+        <p className="font-semibold text-lg text-white truncate" onClick={handleLinkClick}>
+        {linkUrl ? (
+            <a href={linkUrl} target="_blank" rel="noopener noreferrer" >
+              {song.snippet.title}
+            </a>
+          ) : (
+            <span>{song.snippet.title}</span>
+          )}
         </p>
         <p className="mt-1 text-sm text-gray-300 truncate">
           <Link to={`/artists/${song?.snippet.channelId}`}>
-          {song.snippet.channelTitle}
+            {song.snippet.channelTitle}
           </Link>
         </p>
       </div>
     </div>
   );
-}
+};
+
 export default SongCard;
